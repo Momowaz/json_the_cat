1d075e71f8d88a451acf3e51816e9986fac54980
Mohib <43284190+Momowaz@users.noreply.github.com>
2023-04-03 06:37:35 -0700

    Completed cats as a service assignment
    install npm

---
+++ breedFetcher.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const args = process.argv.slice(2);
+
+const name = args[0];
+
+const breedFetcher = function (name) {
+
+    request(`https://api.thecatapi.com/v1/breeds/search?name=${name}`, function (error, res, body) {
+        if (error) {
+            console.error('Request failed');
+            return;
+        }
+        const data = JSON.parse(body);
+        if (data.length === 0) {
+            console.log('Breed not found, please check your URL');
+            return;
+        }
+        console.log('\n');
+        console.log('Description:', data[0].description);
+        console.log('\n');
+    });
+
+}
+
+breedFetcher(name);
